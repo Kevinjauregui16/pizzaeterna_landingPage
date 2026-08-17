d83b74c152a1a3f54abad05e98106a76a96a704b
Kevin Juaregui <krkevinrogelio1600@gmail.com>
2026-08-17 00:13:03 -0600

    feat(global): update number phone

--- src/lib/whatsapp.ts
+++ src/lib/whatsapp.ts
@@ -1,4 +1,4 @@
-const phone = "12395551234";
+const phone = "3327401139";
 const message = "Hola, quisiera hacer un pedido.";
 
 export const whatsappUrl = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
