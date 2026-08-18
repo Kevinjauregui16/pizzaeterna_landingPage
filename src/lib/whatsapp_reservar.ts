da16506fbf55054acc37f1a4edd91cd551ca0731
Kevin Juaregui <krkevinrogelio1600@gmail.com>
2026-08-18 00:04:43 -0600

    feat(global): updates minimise

--- src/lib/whatsapp_reservar.ts
+++ src/lib/whatsapp_reservar.ts
@@ -1,4 +1,4 @@
 const phone = "3327401139";
-const message = "¡Hola! 👋🍕 Me gustaría hacer un pedido 😋";
+const message = "¡Hola! 👋🍕 Me gustaría hacer una reserva ✨";
 
 export const whatsappUrl = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
